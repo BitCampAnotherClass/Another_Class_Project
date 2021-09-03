66b76d3d32e998d6c5fddeec76ea8ce4a08fd7de
jaehyo <84313623+Hyo298@users.noreply.github.com>
2021-09-03 15:43:22 +0900

    TEST

---
+++ Test_JAVA.java
@@ -0,0 +1,12 @@
+
+public class Test_IN_JAVA {
+
+	public Test_IN_JAVA() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("æ»≥Á«œººø‰");
+	}
+
+}
