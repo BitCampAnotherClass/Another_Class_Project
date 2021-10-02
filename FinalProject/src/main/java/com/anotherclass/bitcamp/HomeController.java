811b30cae4d9f526a840aae0339756c87c1c49e2
jaehyo <84313623+Hyo298@users.noreply.github.com>
2021-10-03 01:16:39 +0900

    2021/10/03 01:16
    error404 update

--- FinalProject/src/main/java/com/anotherclass/bitcamp/HomeController.java
+++ FinalProject/src/main/java/com/anotherclass/bitcamp/HomeController.java
@@ -39,7 +39,4 @@ public class HomeController {
 	public String login() {
 		return "/login";
 	}
-	
-
-	
 }
--- FinalProject/src/main/java/com/anotherclass/bitcamp/register/RegisterController.java
+++ FinalProject/src/main/java/com/anotherclass/bitcamp/register/RegisterController.java
@@ -1,13 +1,24 @@
 package com.anotherclass.bitcamp.register;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegisterController {
 	
-	@RequestMapping("/register")
+	@RequestMapping("/register/te")
 	public String register() {
 		return "register/userRegister";
+	}
+	
+	@GetMapping("/error404")
+	public String error404() {
+		return "error/error404";
+	}
+	
+	@RequestMapping("/user/ho")
+	public String test() {
+		return "user/home";
 	}
 }
