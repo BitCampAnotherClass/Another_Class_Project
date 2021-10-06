980939ac7df6733c3beff0aac7ceeb220523b1f2
leecdng <85078379+leecdng@users.noreply.github.com>
2021-10-07 05:47:20 +0900

    211007 0547

--- FinalProject/src/main/java/com/anotherclass/bitcamp/user/UserController.java
+++ FinalProject/src/main/java/com/anotherclass/bitcamp/user/UserController.java
@@ -16,26 +16,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class UserController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home() {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		return "user/home";
 	}
 	
-	// 화면 보려고 임시 맵핑 --------------------------
+	// 임시 맵핑 --------------------------
 	// 은정
 	@RequestMapping(value = "/login")
 	public String login() {
@@ -66,6 +54,7 @@ public class UserController {
 	public String classReview() {
 		return "/user/classDetailPage/test";
 	}
+
 	
 	
 	// 지수
