040ce68ff2a76c2b0359f4caad6267b5ae6702c1
jaehyo <84313623+Hyo298@users.noreply.github.com>
2021-10-08 19:12:09 +0900

    2021/10/08 19:12
    2021/10/08 19:12

--- FinalProject/src/main/java/com/anotherclass/bitcamp/admin/AdminController.java
+++ FinalProject/src/main/java/com/anotherclass/bitcamp/admin/AdminController.java
@@ -4,10 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 	
-	@RequestMapping("/admin")
-	public String creator() {
+	@RequestMapping("/")
+	public String admin() {
 		return "admin/admin";
+	}
+	
+	@RequestMapping(value="/userManagement")
+	public String UserManagement() {
+		return "admin/MemberManagement/userManagement";
+	}
+	
+	@RequestMapping(value="/creatorManagement")
+	public String CreatorManagement() {
+		return "admin/MemberManagement/creatorManagement";
 	}
 }
