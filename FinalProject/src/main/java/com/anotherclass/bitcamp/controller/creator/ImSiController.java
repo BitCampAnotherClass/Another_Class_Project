7430240a5a9462e8612bb66084025d6f36841243
jaehyo <84313623+Hyo298@users.noreply.github.com>
2021-10-21 19:34:09 +0900

    2021/10/21 19:34

--- FinalProject/src/main/java/com/anotherclass/bitcamp/controller/creator/ImSiController.java
+++ FinalProject/src/main/java/com/anotherclass/bitcamp/controller/creator/ImSiController.java
@@ -42,4 +42,10 @@ public class ImSiController {
 		
 		return "creator/question/creatorInquiry";
 	}
+	
+	@RequestMapping("/testingMenu3")
+	public String testing2() {
+		
+		return "creator/board";
+	}
 }
