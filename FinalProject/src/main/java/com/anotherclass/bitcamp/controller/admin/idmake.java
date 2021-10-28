6a5963ffbd2901d701240b5080d27d758fe1e123
jaehyo <84313623+Hyo298@users.noreply.github.com>
2021-10-28 18:58:05 +0900

    2021/10/28 1858

--- FinalProject/src/main/java/com/anotherclass/bitcamp/controller/admin/idmake.java
+++ FinalProject/src/main/java/com/anotherclass/bitcamp/controller/admin/idmake.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.anotherclass.bitcamp.register.HashingSeting;
+import com.anotherclass.bitcamp.register.RegisterVO;
 import com.anotherclass.bitcamp.service.register.RegisterService;
 
 @Controller
@@ -20,24 +21,62 @@ public class idmake {
 	RegisterService registerService;
 	
 	@RequestMapping("/makeIdcheck")
-	public String testing() {
+	public String testing(RegisterVO vo)throws Exception  {
 		String idText="";
-		
-		for (int i = 0; i < 500; i++) { 
+		int check = 0;
+		for (int i = 0; i < 50; i++) { 
 			if(i % 10 == 0) { 
 				System.err.println(); 
 			} 
-			idText=text()+number()+" ";
+			idText=text()+number();
 			System.err.println(idText);
+			vo.setMember_pw(idText);
+			vo.setMember_id(idText);
+			vo.setMember_name(idText);
+			vo.setMember_tel("010"+"-"+num2()+"-"+num3());
+			vo.setMember_email(idText+"@naver.com");
+			vo.setNick(idText);
+			vo.setMember_pw(hashing.setEncryption(idText,idText));
+			//check = registerService.userAccountJoin(vo);
+			//check = registerService.creatorAccountJoin(vo);
+			System.out.println("----------------------");
+			System.out.println(vo.getMember_pw());
+			System.out.println(vo.getMember_id());
+			System.out.println(vo.getMember_name());
+			System.out.println(vo.getMember_tel());
+			System.out.println(vo.getMember_email());
+			System.out.println(vo.getNick());
+			System.out.println("----------------------");
+			System.out.println("업로드중--->"+check);
 		}
+		System.out.println();
 		
 		return idText;
 	}
 	
 	public static String text() {
-		String text = ""; 
+		String text =""; 
 		String ran = "abcdefghijklmnopqrstuvwxyz";
 		for(int i = 0; i < 6; i++) { 
+			text += ran.charAt((int)(Math.random() * ran.length())); 
+		} 
+		return text;
+	}
+	
+	
+	public static String num2() {
+		String text = ""; 
+		String ran = "123456789";
+		for(int i = 0; i < 4; i++) { 
+			text += ran.charAt((int)(Math.random() * ran.length())); 
+		} 
+		return text;
+	}
+	
+	public static String num3() {
+		String text = ""; 
+		String ran = "0123456789";
+		for(int i = 0; i < 4; i++) { 
 			text += ran.charAt((int)(Math.random() * ran.length())); 
 		} 
 		return text;
